5570e3acffada820b0e460dcd5e4f9ee1e7f8d6e
Steve Wallis <steve.wallis@gmail.com>
2015-01-26 23:46:16 +0100

    tabs to spaces

--- chip8.c
+++ chip8.c
@@ -27,8 +27,8 @@ uint8_t drawFlag = 0;
 
 void init() {
     I = 0, sp = 0;
-	timer_delay = 0, timer_sound = 0;
-	PC = 0x200; //program always starts here in memory
+    timer_delay = 0, timer_sound = 0;
+    PC = 0x200; //program always starts here in memory
     op = 0;
     memcpy(&memory[C8_FONTSET_BASE], chip8_fontset, C8_FONTSET_SIZE);
 }
@@ -55,10 +55,10 @@ void C8_Lookup_8xxx(){
 }
 
 void C8_Lookup_Exxx(){
-  if ((op&0x00ff) == 0x9E) 
-		C8_OP_Ex9E();
-  else 
-		C8_OP_ExA1();
+    if ((op&0x00ff) == 0x9E) 
+        C8_OP_Ex9E();
+    else 
+        C8_OP_ExA1();
 }
 
 void C8_Lookup_Fxxx(){
@@ -96,57 +96,57 @@ void C8_Lookup_Fxxx(){
 }
 
 void C8_OP_00EE() { // RET
-	PC = stack[sp]; 
-	sp--; 
+    PC = stack[sp]; 
+    sp--; 
 }
 
 void C8_OP_1nnn(){ // JMP
-	PC = op&0x0fff; 
+    PC = op&0x0fff; 
 }
 
 void C8_OP_2nnn(){ // CALL
-	sp++; 
-	stack[sp] = PC; 
-	PC = op&0x0fff; 
+    sp++; 
+    stack[sp] = PC; 
+    PC = op&0x0fff; 
 }
 
 void C8_OP_3xnn(){ //SE x nn
-	if (V[(op&0x0f00)>>8] == (op&0x00ff)) 
-		PC+=2; 
+    if (V[(op&0x0f00)>>8] == (op&0x00ff)) 
+        PC+=2; 
 }
 
 void C8_OP_4xnn(){ // SNE x nn
-	if (V[(op&0x0f00)>>8] != (op&0x00ff)) 
-		PC+=2;
+    if (V[(op&0x0f00)>>8] != (op&0x00ff)) 
+        PC+=2;
 }
 
 void C8_OP_5xy0(){ // SE x y
-	if (V[(op&0x0f00)>>8] == V[(op&0x00f0)>>4]) 
-		PC+=2;
+    if (V[(op&0x0f00)>>8] == V[(op&0x00f0)>>4]) 
+        PC+=2;
 }
 
 void C8_OP_6xnn(){ //MOV x nn
-	V[(op&0x0f00)>>8] = (op&0x00ff);
+    V[(op&0x0f00)>>8] = (op&0x00ff);
 }
 
 void C8_OP_7xnn(){ // ADD x nn
-	V[(op&0x0f00)>>8] += (op&0x00ff);
+    V[(op&0x0f00)>>8] += (op&0x00ff);
 }
 
 void C8_OP_8xy0() { // MOV x y
-	V[(op&0x0f00)>>8] = V[(op&0x00f0)>>4];
+    V[(op&0x0f00)>>8] = V[(op&0x00f0)>>4];
 }
 
 void C8_OP_8xy1() { // OR x y
-	V[(op&0x0f00)>>8] = V[(op&0x0f00)>>8]|V[(op&0x00f0)>>4];
+    V[(op&0x0f00)>>8] = V[(op&0x0f00)>>8]|V[(op&0x00f0)>>4];
 }
 
 void C8_OP_8xy2() { // AND x y
-	V[(op&0x0f00)>>8] = V[(op&0x0f00)>>8]&V[(op&0x00f0)>>4];
+    V[(op&0x0f00)>>8] = V[(op&0x0f00)>>8]&V[(op&0x00f0)>>4];
 }
 
 void C8_OP_8xy3() { // XOR x y
-	V[(op&0x0f00)>>8] = V[(op&0x0f00)>>8]^V[(op&0x00f0)>>4];
+    V[(op&0x0f00)>>8] = V[(op&0x0f00)>>8]^V[(op&0x00f0)>>4];
 }
 
 void C8_OP_8xy4() { // ADD x y 
@@ -176,15 +176,15 @@ void C8_OP_8xyE() { // SHL x
 }
 
 void C8_OP_9xy0() { // SNE x y
-	if (V[(op&0x0f00)>>8] != V[(op&0x00f0)>>4]) PC+=2;
+    if (V[(op&0x0f00)>>8] != V[(op&0x00f0)>>4]) PC+=2;
 }
 	
 void C8_OP_Annn() { // MOV I
-	I = op&0x0fff; 
+    I = op&0x0fff; 
 }
 
 void C8_OP_Bnnn() { // JMP V0
-	PC = op&0x0fff + V[0]; 
+    PC = op&0x0fff + V[0]; 
 }
 
 void C8_OP_Cxnn() { // RND
@@ -192,7 +192,7 @@ void C8_OP_Cxnn() { // RND
 }
 
 void C8_OP_Dxyn() { // DRW x y n
-	//printf("%04x : DRW NOT DONE YET\n",op);
+    //printf("%04x : DRW NOT DONE YET\n",op);
     uint8_t n,x,y;
     n = op&0xf; //number of bytes (ie height of sprite).
     x = V[(op&0x0f00)>>8];
@@ -205,31 +205,31 @@ void C8_OP_Dxyn() { // DRW x y n
 }
 
 void C8_OP_Ex9E() { // SKP
-	if (keyboard[V[(op&0x0f00)>>8]]) PC+=2; 
+    if (keyboard[V[(op&0x0f00)>>8]]) PC+=2; 
 }
 
 void C8_OP_ExA1() { // SKNP
-	if (!keyboard[V[(op&0x0f00)>>8]]) PC+=2; 
+    if (!keyboard[V[(op&0x0f00)>>8]]) PC+=2; 
 }
 
 void C8_OP_Fx07() { // MOV DT x
-	V[(op&0x0f00)>>8] = timer_delay; 
+    V[(op&0x0f00)>>8] = timer_delay; 
 }
 
 void C8_OP_Fx0A() { // WAITKEY
-	printf("%04x : WAITKEY NOT DONE YET\n",op);
+    printf("%04x : WAITKEY NOT DONE YET\n",op);
 }
 
 void C8_OP_Fx15() { // MOV x DT 
-	timer_delay = V[(op&0x0f00)>>8];
+    timer_delay = V[(op&0x0f00)>>8];
 }
 
 void C8_OP_Fx18() { //MOV x ST
-	timer_sound = V[(op&0x0f00)>>8];
+    timer_sound = V[(op&0x0f00)>>8];
 }
 
 void C8_OP_Fx1E() { // MOV ADD I
-	I += V[(op&0x0f00)>>8]; 
+    I += V[(op&0x0f00)>>8]; 
 }
 
 void C8_OP_Fx29() { // LD F
@@ -237,18 +237,18 @@ void C8_OP_Fx29() { // LD F
 }
 
 void C8_OP_Fx33() { // LD B
-	printf("%04x : LD B NOT DONE YET\n",op);
+    printf("%04x : LD B NOT DONE YET\n",op);
 }
 
 void C8_OP_Fx55() { // LD MEM[I] V0Vx
-	uint8_t x = (op&0x0f00)>>8;
+    uint8_t x = (op&0x0f00)>>8;
     for (uint8_t i=0;i<=x;i++) {
         memory[I+i] = V[i]; 
     }
 }
 
 void C8_OP_Fx65() { // LD V0Vx MEM[I]
-	uint8_t x = (op&0x0f00)>>8;
+    uint8_t x = (op&0x0f00)>>8;
     for (uint8_t i=0;i<=x;i++) {
         V[i] = memory[I+i]; 
     }
@@ -262,9 +262,9 @@ void C8_tick() {
         C8_OP_Cxnn, C8_OP_Dxyn, C8_Lookup_Exxx, C8_Lookup_Fxxx,
         C8_OP_Invalid};
 
-	op = memory[PC] << 8 | memory[PC + 1];
+    op = memory[PC] << 8 | memory[PC + 1];
     PC+=2;
-	C8_OPTable[(op&0xf000)>>12]();
+    C8_OPTable[(op&0xf000)>>12]();
 }
 
 int load(char *file) {
@@ -283,22 +283,22 @@ int main(int argc, char **argv) {
         return 1;
     }
     init();	
-	load(argv[1]);
-	
-	int W = 640;
-	int H = 320;
-	
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* win = SDL_CreateWindow("Chip8", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, W, H, SDL_WINDOW_SHOWN);
-	SDL_Surface* surface = SDL_GetWindowSurface(win);
-	
+    load(argv[1]);
+    
+    int W = 640;
+    int H = 320;
+    
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* win = SDL_CreateWindow("Chip8", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, W, H, SDL_WINDOW_SHOWN);
+    SDL_Surface* surface = SDL_GetWindowSurface(win);
+    
     SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xff, 0xff, 0xff));
     SDL_UpdateWindowSurface(win);
 
     //SDL_Renderer* renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-	//SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
-	/**/
-	SDL_Event e;	
+    //SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+    /**/
+    SDL_Event e;	
     for(;;) {
         while(SDL_PollEvent(&e) != 0) {
             switch (e.type) {
@@ -351,7 +351,7 @@ int main(int argc, char **argv) {
             }
         }
 
-	    C8_tick();
+        C8_tick();
         if (drawFlag) {
 
             drawFlag = 0;
@@ -364,6 +364,6 @@ int main(int argc, char **argv) {
     SDL_FreeSurface(surface);
     SDL_DestroyWindow(win);
     SDL_Quit();
-	
-	return 0;
+
+    return 0;
 }
